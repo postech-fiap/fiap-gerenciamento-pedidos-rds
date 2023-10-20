c6bf7612dbff8461d9b1f8085ed25a47dbcc30bf
icaarodias <icaaro.dias@gmail.com>
2023-10-20 17:36:43 -0300

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -1,6 +1,28 @@
 #main.tf
 #defining the provider as aws
+#Fiap-gerenciamento-pedidos-pos-tech
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "postech-fiap"
+
+    workspaces {
+      name = "gh-actions"
+    }
+  }
+}
 
 provider "aws" {
     region     = "${var.region}"
