4e7430b3bdfe08cd699a1cedbbf8a6a848f59da5
icaarodias <icaaro.dias@gmail.com>
2023-11-01 18:24:15 -0300

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,1 +1,25 @@
+#provider.tf
+#defining the provider as aws
+#Fiap-gerenciamento-pedidos-pos-tech
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "postech-fiap"
+
+    workspaces {
+      name = "gh-actions"
+    }
+  }
+}
