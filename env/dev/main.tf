b2af2fbfd068e1c4d240d6986f32cfa7406edcae
icaarodias <icaaro.dias@gmail.com>
2023-11-01 21:54:12 -0300

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,3 +1,6 @@
 module "aws-dev" {
   source = "../../infra"
+  AWS_SECRET_ACCESS_KEY = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  AWS_ACCESS_KEY_ID =  ${{ secrets.AWS_ACCESS_KEY_ID }}
+  AWS_REGION = ${{ secrets.AWS_REGION }}
 }
