6ac2f92213c8fa7e4c65597486b59802e1db82f9
icaarodias <icaaro.dias@gmail.com>
2023-11-01 21:57:26 -0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,10 +1,1 @@
-#variables.tf
-variable "AWS_SECRET_ACCESS_KEY" {
-    description = "Access key to AWS console"
-}
-variable "AWS_ACCESS_KEY_ID" {
-    description = "Secret key to AWS console"
-}
-variable "AWS_REGION" {
-    description = "AWS region"
-}
+
