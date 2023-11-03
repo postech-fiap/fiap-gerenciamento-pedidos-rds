635258600bd20647664f8e51a477f02f37fc66a2
icaarodias <icaaro.dias@gmail.com>
2023-11-03 17:24:40 -0300

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,6 +7,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.0.0"
+      configuration_aliases = [ aws.alternate ]
+s
     }
     random = {
       source  = "hashicorp/random"
